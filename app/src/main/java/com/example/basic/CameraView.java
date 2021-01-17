30b90f0ffd21016ec0b9066eb82a980971d9eabc
minakhan01 <minakhan01@gmail.com>
2021-01-17 15:21:09 -0500

    camera view working, removed new api stuff

---
+++ app/src/main/java/com/example/basic/CameraView.java
@@ -0,0 +1,72 @@
+package com.example.basic;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class CameraView extends SurfaceView implements SurfaceHolder.Callback {
+    private Camera camera = null;
+
+    public CameraView(Context context) {
+        super(context);
+
+        final SurfaceHolder surfaceHolder = getHolder();
+        surfaceHolder.addCallback(this);
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        camera = Camera.open();
+
+        // Set the Hotfix for Google Glass
+        setCameraParameters(camera);
+
+        // Show the Camera display
+        try {
+            camera.setPreviewDisplay(holder);
+        } catch (Exception e) {
+            releaseCamera();
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        // Start the preview for surfaceChanged
+        if (camera != null) {
+            camera.startPreview();
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // Do not hold the camera during surfaceDestroyed - view should be gone
+        releaseCamera();
+    }
+
+    /**
+     * Important HotFix for Google Glass (post-XE11) update
+     *
+     * @param camera Object
+     */
+    public void setCameraParameters(Camera camera) {
+        if (camera != null) {
+            final Camera.Parameters parameters = camera.getParameters();
+            parameters.setPreviewFpsRange(30000, 30000);
+            camera.setParameters(parameters);
+        }
+    }
+
+    /**
+     * Release the camera from use
+     */
+    public void releaseCamera() {
+        if (camera != null) {
+            camera.release();
+            camera = null;
+        }
+    }
+}
+
