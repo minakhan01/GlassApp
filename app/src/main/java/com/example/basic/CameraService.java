24056544531393ff3d9a181a7843e669e8ea1abb
minakhan01 <minakhan01@gmail.com>
2021-01-23 07:33:17 -0500

    addded wake lock to keep app on

--- app/src/main/java/com/example/basic/CameraService.java
+++ app/src/main/java/com/example/basic/CameraService.java
@@ -1,9 +1,11 @@
 package com.example.basic;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.IBinder;
+import android.os.PowerManager;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -11,6 +13,7 @@ import android.widget.Toast;
 
 public class CameraService extends Service {
     private CameraView cameraView = null;
+    protected PowerManager.WakeLock mWakeLock;
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
@@ -18,6 +21,9 @@ public class CameraService extends Service {
         cameraView = new CameraView(this);
         TakeIntervalPicture();
         Log.d("Camera Service", "onStartCommand");
+        final PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        mWakeLock = pm.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK, "basic:wakelock");
+        mWakeLock.acquire();
         // If we get killed, after returning from here, restart
         return START_STICKY;
     }
@@ -48,6 +54,7 @@ public class CameraService extends Service {
     // stop on calling this method
     public void onDestroy() {
         Log.d("Camera Service", "onDestroy");
+        this.mWakeLock.release();
         super.onDestroy();
 
     }
